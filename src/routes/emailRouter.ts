7c92d59730c9de1d5f6f4b9cab0864a905bed771
Shokhrookh Ruziev <shokhrukh.ruziyev@arca.uz>
2025-09-26 18:06:16 +0500

    feat: implement HTTP status constants and update emailRouter to use them

--- src/routes/emailRouter.ts
+++ src/routes/emailRouter.ts
@@ -1,6 +1,7 @@
 import {Router} from 'express';
 import nodemailer from 'nodemailer';
 import {EmailService} from '../domain';
+import {HTTP_STATUSES} from '../constants/httpStatuses';
 export const getEmailRoutes = () => {
   const router = Router();
   const emailService = new EmailService();
@@ -8,7 +9,7 @@ export const getEmailRoutes = () => {
     const sentEmail = await emailService.sendPasswordRecoveryEmail();
 
     if (!sentEmail) {
-      res.status(401).send('Email not sent');
+      res.status(HTTP_STATUSES.NOT_FOUND_404).send('Email not sent');
       return;
     }
     res.send('Email sent successfully');
