e5cea5b18e4f039616f15cd7e18b87928791163d
Shokhrookh Ruziev <shokhrukh.ruziyev@arca.uz>
2025-10-29 14:39:47 +0500

    refactor: update docker-compose and package.json for improved service configuration and script execution

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,33 @@
 import express, {NextFunction, Request, Response} from 'express';
 import {
-  getAuthRoutes,
-  getClubRoutes,
-  getProductRoutes,
-  getFeedbackRoutes,
-  getEmailRoutes,
+    getAuthRoutes,
+    getClubRoutes,
+    getProductRoutes,
+    getFeedbackRoutes,
+    getEmailRoutes,
 } from './routes';
 import {runDB} from './repositories/db';
 import dotenv from 'dotenv';
+
 dotenv.config();
 const app = express();
-const PORT = 3000;
+const PORT = process.env.APP_PORT || 3000;
 let requestCounter = 0;
 const bodyMiddleware = express.json();
 const requestCounterMiddleware = (
-  req: Request,
-  res: Response,
-  next: NextFunction
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  requestCounter++;
-  console.log(`Request #${requestCounter} - ${req.method} ${req.url}`);
-  next();
+    requestCounter++;
+    console.log(`Request #${requestCounter} - ${req.method} ${req.url}`);
+    next();
 };
 app.use(bodyMiddleware);
 app.use(requestCounterMiddleware);
 app.get('/', (req, res) => {
-  console.log('Hello World Kuku');
-  res.status(200).send({message: 'Hello World Kuku'});
+    console.log('Hello World Kuku');
+    res.status(200).send({message: 'Hello World Kuku'});
 });
 app.use('/clubs', getClubRoutes());
 app.use('/products', getProductRoutes());
@@ -35,9 +36,9 @@ app.use('/feedbacks', getFeedbackRoutes());
 app.use('/email', getEmailRoutes());
 
 const startApp = async () => {
-  await runDB();
-  app.listen(PORT, () => {
-    console.log(`Server is running on port:${PORT}`);
-  });
+    await runDB();
+    app.listen(PORT, () => {
+        console.log(`Server is running on port:${PORT}`);
+    });
 };
 startApp();
